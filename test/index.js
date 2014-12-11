e7aced98c106447d1644a90e5ed5548525d34ed0
Scott Corgan <scottcorgan@gmail.com>
2014-12-11 14:17:37 -0800

    parse file contents with injectors

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var fs = require('fs-extra');
 
+var _ = require('lodash');
 var test = require('tape');
 var concat = require('concat-stream');
 var split = require('split');
@@ -56,14 +57,14 @@ test('default: adding injector', function (t) {
   t.end();
 });
 
-test('blocks: streaming', function (t) {
+test('block tree: streaming', function (t) {
   
   var d = deposit();
   var blockNum = 0;
   
   var s = fs.createReadStream(TEST1_FILE_PATH)
     .pipe(split())
-    .pipe(d.blocks())
+    .pipe(d.blockTree())
     .pipe(spy.obj(function (line) {
       
       blockNum += 1;
@@ -76,18 +77,18 @@ test('blocks: streaming', function (t) {
     }));
 });
 
-test('blocks: callback', function (t) {
+test('block tree: callback', function (t) {
   
   var d = deposit();
   
-  d.blocks(TEST1_FILE_PATH, function (err, blocks) {
+  d.blockTree(TEST1_FILE_PATH, function (err, blocks) {
     
     t.deepEqual(blocks, TEST1_FILE_BLOCKS, 'parsed blocks object');
     t.end();
   });
 });
 
-test('injection: inject file streaming', function (t) {
+test('tree: streaming', function (t) {
   
   var d = deposit();
   var blockNum = 0;
@@ -95,7 +96,7 @@ test('injection: inject file streaming', function (t) {
   
   var s = fs.createReadStream(TEST1_FILE_PATH)
     .pipe(split())
-    .pipe(d.file())
+    .pipe(d.tree())
     .pipe(spy.obj(function (line) {
       
       if (line.type === 'block') {
@@ -114,11 +115,24 @@ test('injection: inject file streaming', function (t) {
     }));
 });
 
-test.skip('injection: inject file with callback', function (t) {
+test('tree: callback', function (t) {
   
   var d = deposit();
   
-  d.injector('test', function (options, done) {
+  d.tree(TEST1_FILE_PATH, function (err, content) {
+    
+    t.equal(content.length, 16, 'parsed all lines');
+    t.equal(_.filter(content, {type: 'block'}).length, 2, 'block count');
+    t.equal(_.filter(content, {type: 'content'}).length, 14, 'content count');
+    t.end();
+  });
+});
+
+test('injecting: streaming', function (t) {
+  
+  var depositor = deposit();
+  
+  depositor.injector('test', function (options, done) {
     
     t.deepEqual(options, {
       key1: 'value1',
@@ -127,7 +141,7 @@ test.skip('injection: inject file with callback', function (t) {
     
     done(null, '<!-- injected test -->');
   });
-  d.injector('fetch', function (options, done) {
+  depositor.injector('fetch', function (options, done) {
     
     t.deepEqual(options, {
       url: 'http://google.com',
@@ -154,11 +168,63 @@ test.skip('injection: inject file with callback', function (t) {
     '  ',
     '</body>',
     '</html>'
+  ].join('');
+  
+  fs.createReadStream(TEST1_FILE_PATH)
+    .pipe(split())
+    .pipe(depositor)
+    .pipe(concat(function (html) {
+      
+      t.equal(html.toString(), expected, 'injected content');
+      t.end();
+    }));
+});
+
+test('injecting: callback', function (t) {
+  
+  var depositor = deposit();
+  
+  depositor.injector('test', function (options, done) {
+    
+    t.deepEqual(options, {
+      key1: 'value1',
+      key2: 'value2'
+    }, 'passed options into injector');
+    
+    done(null, '<!-- injected test -->');
+  });
+  depositor.injector('fetch', function (options, done) {
+    
+    t.deepEqual(options, {
+      url: 'http://google.com',
+      target: 'data'
+    }, 'passed options into injector');
+    
+    done(null, '<!-- injected fetch -->');
+  });
+  
+  var expected = [
+    '<!DOCTYPE html>',
+    '<html>',
+    '<head>',
+    '  <meta charset="utf-8">',
+    '  <meta http-equiv="X-UA-Compatible" content="IE=edge">',
+    '  <title></title>',
+    '  ',
+    '<!-- injected test -->',
+    '  ',
+    '</head>',
+    '<body>',
+    '  ',
+    '<!-- injected fetch -->',
+    '  ',
+    '</body>',
+    '</html>'
   ].join('\n');
   
-  d.file(TEST1_FILE_PATH, function (err, content) {
+  depositor.parse(TEST1_FILE_PATH, function (err, content) {
     
-    t.equal(content, expected, 'injected contents');
+    t.equal(content, expected, 'content parsed');
     t.end();
   });
 });
